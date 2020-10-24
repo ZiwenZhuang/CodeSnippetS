bf42ad2918f32c581621e8043c1ce1b014a8d4a9
Leo Zhuang <zhuangzw@shanghaitech.edu.cn>
2020-10-24 15:00:43 +0800

    Create tensorflow.py

---
+++ tensorflow.py
@@ -0,0 +1,17 @@
+def is_depend_on(a, b):
+    """ Determine whether `a` depends on `b`
+    return Bool
+    """
+    if a.graph is not b.graph:
+        return False
+    gd = a.graph.as_graph_def()
+    gd_sub = tf.graph_util.extract_sub_graph(gd, [a.op.name])
+    return b.op.name in {n.name for n in gd_sub.node}
+    
+def get_all_parents(a):
+    """ Return a list of names where a depends on all these variables.
+    """
+    gd = a.graph.as_graph_def()
+    gd_sub = tf.graph_util.extract_sub_graph(gd, [a.op.name])
+    return [n.name for n in gd_sub.node]
+    
