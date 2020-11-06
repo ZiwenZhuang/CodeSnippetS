46653321c435401d15b77e3eb7cb0d825e368df3
Leo Zhuang <zhuangzw@shanghaitech.edu.cn>
2020-11-06 13:07:28 +0800

    Update tensorflow.py

--- tensorflow.py
+++ tensorflow.py
@@ -15,7 +15,14 @@ def get_all_parents(a):
     gd_sub = tf.graph_util.extract_sub_graph(gd, [a.op.name])
     return [n.name for n in gd_sub.node]
     
-def get_all_variables(scope):
+def get_all_variables(scope= None):
     """ Return all variables that belong to this scope.
+    @Args:
+        scope: str, the name of this scope, if not provided, all variables in current system will be returned
     """
     return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope= scope)
+
+def get_current_scope_name():
+    """ Return the current tensorflow variable scope name
+    """
+    return tf.get_variable_scope().name
